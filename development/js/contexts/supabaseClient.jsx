55fad716d95947e6e67b706623e7299953afc556
PaulinaRog <rog.paulina@gmail.com>
2022-12-12 22:46:46 +0100

    supaUrl

--- development/js/contexts/supabaseClient.jsx
+++ development/js/contexts/supabaseClient.jsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseUrl = "https://vgrtdhqwzgkegugwynsl.supabase.co";
 const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
